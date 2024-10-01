4b00fc19dba1ac88921a60d6b23da959c00ace19
ozantalay <ozntalay@hotmail.com>
2024-10-01 16:18:29 +0300

    baslangic

---
+++ src/components/DebatePost.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import PostComments from './PostComments'
+import PostContent from './PostContent'
+import postData from '../postData'
+
+export default function DebatePost() {
+  /* Challenge 
+
+Form çalışmıyor. Göreviniz, kullanıcı "Gönder "e tıkladığında gönderiye bir yorum ekleyen kontrollü bir form yapmaktır.
+
+    1. Yorum, yorum dizisinin alt kısmında, girilen kullanıcı adı ve yorum metni önceki yorumlar gibi görüntülenecek şekilde görünmelidir. 
+       
+    2. Yorum, önceki yorumların verilerini içeren array'e eklenmelidir. 
+    
+    3. Girilen kullanıcı adı kaydedilmeli, ancak kullanıcı onay kutusunu işaretlerse "AnonimKullanıcı" olarak görünmelidir.
+    
+    4. Kullanıcı formu göndermek için text input elemanına ve comment box elemanına metin girmek zorunda olmalı ve kullanıcı bir yorum gönderdikten sonra elemanlar ve onay kutusu temizlenmelidir. Sayfa yüklendiğinde de boş olmalıdırlar.   
+        
+    5. Kodunuz tamamen bu dosyanın içinde yer alabilir, ancak isterseniz bazı kısımları taşıyabilirsiniz. 
+
+*/
+
+  const [comments, setComments] = useState(postData.comments)
+
+  return (
+    <div className='post-container'>
+      <PostContent data={{ ...postData }} />
+      <PostComments data={comments} />
+      <form>
+        <input
+          className='text-input'
+          type='text'
+          placeholder='Kullanıcı adı girin.'
+        />
+        <textarea placeholder='Ne düşünüyorsunuz?' />
+        <label>
+          <input className='checkbox' type='checkbox' />
+          İsimsiz mi göndereyim?
+        </label>
+        <button>Gönder</button>
+      </form>
+    </div>
+  )
+}
